0819452d02ad208199dd5fe2c813aa7762e9789f
kingofthetoxicclowns <120647462+kingofthetoxicclowns@users.noreply.github.com>
2022-12-23 22:27:12 +0700

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,21 @@
+function checkLogin(value) {
+    if (value == "") {
+      alert("Enter login!");
+    } else if (value.indexOf("@") == -1) {
+      alert('Missing character "@"');
+    } else if (value.indexOf(".") == -1) {
+      alert('Missing character "." in email address');
+    }
+  }
+  function checkPassword(value) {
+    let r = /[^A-Z-a-z-0-9]/g;
+    if (value == "") {
+      alert("Enter password!");
+    } else if (value.length < 6) {
+      alert("The password is too short!");
+    } else if (value.length > 20) {
+      alert("The password is too long!");
+    } else if (r.test(value)) {
+      alert("Invalid characters in the password");
+    }
+  }
